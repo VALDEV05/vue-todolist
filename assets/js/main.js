ed0546e43fa697e7971ad1e28ce3ef5bd0a97e20
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-11-02 12:20:52 +0100

    Created the task array and printed to screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,25 @@
+/* 
+    To do List:
+        //-stampare in pagina un item per ogni elemento contenuto in un array
+        -ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+        -predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista */
+
+/*  Funzionalitá di Base:
+        -La nostra todo list avrá alcune tasks di default predefinite
+        -L'utente puó inserire nuove tasks
+        -Cliccando sulla "X" l'utente puó cancellare una task
+        -Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+        -ma vuole anche poter indicare che la task é stata completata (con un icona cliccabile)
+        -Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+        -Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri. */
 var app = new Vue({
     el: '#app',
+    data: {
+        tasks: [
+            'Fare la spesa',
+            'Porta fuori il cane',
+            'Studia VueJS',
+            'controlla la To Do List'
+        ]
+    }
 })
