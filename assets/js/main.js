7416102e453b68df6b32c29f41409f83239a81ba
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-11-02 13:56:58 +0100

    Added check that if there are no more tasks in the list, show a message like "Nothing to do"

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,17 +5,17 @@
         //-predisporre un input per aggiungere un nuovo item alla lista: 
             //digitando il tasto invio oppure ciccando su un pulsante 
             //il testo digitato viene aggiunto alla lista 
-        se queste regole 
-            controllare la lunghezza minima della stringa della newTask (5 caratteri)
-            controllare se realmente sono stati digitati caratteri
-        non vengono rispettate allora non pusho nulla
+        //se queste regole 
+            //controllare la lunghezza minima della stringa della newTask (5 caratteri)
+            //controllare se realmente sono stati digitati caratteri
+        //non vengono rispettate allora mostro un messaggio di errore
         */
 
 /*  Funzionalitá di Base:
-        -La nostra todo list avrá alcune tasks di default predefinite
-        -L'utente puó inserire nuove tasks
-        -Cliccando sulla "X" l'utente puó cancellare una task
-        -Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+        //-La nostra todo list avrá alcune tasks di default predefinite
+        //-L'utente puó inserire nuove tasks
+        //-Cliccando sulla "X" l'utente puó cancellare una task
+        //-Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
         -ma vuole anche poter indicare che la task é stata completata (con un icona cliccabile)
         -Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
         -Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri. */
